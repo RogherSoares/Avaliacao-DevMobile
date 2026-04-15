739a10a22fc513f0b2e5e9501b33c6ca48303fef
Rogher Adriano Soares <rogher_adriano@hotmail.com>
2026-04-15 20:57:08 -0300

    criando a logica de insercao no database

--- app/src/main/java/com/roghersoares/avaliacaodevmobile/ProdutoDao.java
+++ app/src/main/java/com/roghersoares/avaliacaodevmobile/ProdutoDao.java
@@ -1,4 +1,15 @@
 package com.roghersoares.avaliacaodevmobile;
 
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface ProdutoDao {
+    @Insert
+    void insert(Produto produto);
+    @Query("SELECT * FROM produto")
+    List<Produto> getAllProdutos();
 }
