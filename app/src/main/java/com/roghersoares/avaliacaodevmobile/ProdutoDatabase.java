900a3d22cc1d74ba98acdccc3f7b652ad2846fa3
Rogher Adriano Soares <rogher_adriano@hotmail.com>
2026-04-15 20:54:06 -0300

    criando o database room

--- app/src/main/java/com/roghersoares/avaliacaodevmobile/ProdutoDatabase.java
+++ app/src/main/java/com/roghersoares/avaliacaodevmobile/ProdutoDatabase.java
@@ -1,4 +1,21 @@
 package com.roghersoares.avaliacaodevmobile;
 
-public class ProdutoDatabase {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Produto.class}, version = 1)
+public abstract class ProdutoDatabase extends RoomDatabase {
+    private static ProdutoDatabase instance;
+
+    public abstract ProdutoDao produtoDao();
+
+    public static synchronized ProdutoDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(), ProdutoDatabase.class, "produto_database").fallbackToDestructiveMigration().allowMainThreadQueries().build();
+        }
+        return instance;
+    }
 }
