0ef96df0c46551004e65d5dcdc69f1d815c8c423
Rogher Adriano Soares <rogher_adriano@hotmail.com>
2026-04-15 19:48:20 -0300

    Definindo o layout da pagina principal e declarando os campos de entradas de dados e criando o objeto de interacao com o banco de dados

--- app/src/main/java/com/roghersoares/avaliacaodevmobile/MainActivity.java
+++ app/src/main/java/com/roghersoares/avaliacaodevmobile/MainActivity.java
@@ -1,24 +1,21 @@
 package com.roghersoares.avaliacaodevmobile;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    //Declarando os campos de entrada de dados
+    private EditText editTextProduto, editTextCodigoProduto, editTextPreco, editTextQuantidade;
+    //Objeto para interação com o banco de dados (DAO)
+    private ProdutoDao produtoDao;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_main); // Define o layout da atividade
+
+
     }
 }
