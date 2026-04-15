e5567a52039fbb3c69f1d22828fbbf0bac17550e
Rogher Adriano Soares <rogher_adriano@hotmail.com>
2026-04-15 20:35:26 -0300

    logica de conexao com o banco de dados

--- app/src/main/java/com/roghersoares/avaliacaodevmobile/ReportActivity.java
+++ app/src/main/java/com/roghersoares/avaliacaodevmobile/ReportActivity.java
@@ -22,6 +22,30 @@ public class ReportActivity extends AppCompatActivity {
         Button btnVoltar = findViewById(R.id.btnVoltar);
         //O botão de retorno utilizando expressão lambda
         btnVoltar.setOnClickListener(v -> voltarParaCadastro());
+
+        /*
+         * Conexão com o banco de dados
+         * 1- Cria uma instância do banco "user-database"
+         * 2-.alloeMainThreadQueries(): serve para liberar operações, ROOM proíbe isso.
+         * O correto seria fazer consultas em threads separados.
+         */
+
+        ProdutoDatabase db = ProdutoDatabase.getInstance(getApplicationContext());
+        //Ontém o objeto DAO (Data Access Object) que contém as queries SQL
+        ProdutoDao produtoDao = db.produtoDao();
+        //Recupera todos os produtos salvos no BD e armazena numa lista
+        java.util.List<Produto> produtoList = produtoDao.getAllProdutos();
+        //StringBuilder: forma eficiente de construir uma String longa dentro de um laço de repetição
+        StringBuilder report = new StringBuilder();
+        //Loop "for-each" para percorrer cada objeto Produto na lista
+        for (Produto produto : produtoList) {
+            report.append("Nome do Produto: ").append(produto.getNomeProduto()).append("\n")
+                    .append("Código do Produto: ").append(produto.getCodigoProduto()).append("\n")
+                    .append("Preço: ").append(produto.getPreco()).append("\n")
+                    .append("Quantidade: ").append(produto.getQuantidade()).append("\n\n");
+        }
+        //Exibe o relatório no campo de texto
+        editTextRelatorio.setText(report.toString());
     }
 
     //Método para voltar para a tela de cadastro
