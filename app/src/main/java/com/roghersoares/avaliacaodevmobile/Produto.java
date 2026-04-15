b7f59841b0153e086a175bb05d581d51802d19e2
Rogher Adriano Soares <rogher_adriano@hotmail.com>
2026-04-15 20:49:24 -0300

    criando a classe produto e seus getters e setters

--- app/src/main/java/com/roghersoares/avaliacaodevmobile/Produto.java
+++ app/src/main/java/com/roghersoares/avaliacaodevmobile/Produto.java
@@ -1,4 +1,56 @@
 package com.roghersoares.avaliacaodevmobile;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Produto {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String nomeProduto;
+    private String codigoProduto;
+    private String preco;
+    private String quantidade;
+
+    public Produto(String nomeProduto, String codigoProduto, String preco, String quantidade) {
+        this.nomeProduto = nomeProduto;
+        this.codigoProduto = codigoProduto;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+    // Getters e setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getCodigoProduto() {
+        return codigoProduto;
+    }
+    public void setCodigoProduto(String codigoProduto) {
+        this.codigoProduto = codigoProduto;
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+
+    public String getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(String quantidade) {
+        this.quantidade = quantidade;
+    }
 }
