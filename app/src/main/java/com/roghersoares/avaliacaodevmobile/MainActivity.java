c27be5ca8cdefbd7bbb80e776811332930e3b40e
Rogher Adriano Soares <rogher_adriano@hotmail.com>
2026-04-15 20:03:48 -0300

    Configurar o Banco de Dados e o Botao Salvar Produtos

--- app/src/main/java/com/roghersoares/avaliacaodevmobile/MainActivity.java
+++ app/src/main/java/com/roghersoares/avaliacaodevmobile/MainActivity.java
@@ -1,6 +1,7 @@
 package com.roghersoares.avaliacaodevmobile;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,5 +26,23 @@ public class MainActivity extends AppCompatActivity {
         //Inicializa os Botões
         Button buttonSalvar = findViewById(R.id.buttonSalvar);
         Button buttonRelatorio = findViewById(R.id.buttonRelatorio);
+
+        //Configuração do Banco de Dados Utilizando o Room
+            ProdutoDatabase db = ProdutoDatabase.getInstance(getApplicationContext());
+            produtoDao = db.produtoDao(); //Obtém uma instância do DAO para interagir com os dados
+
+            //Configura o botão de salvar produto
+            buttonSalvar.setOnClickListener(v -> {
+                //Confirma que o click ocorreu
+                Log.d("MainActivity", "Botão Salvar clicado")
+                //Obtém os valores digitados pelo usuário
+                String nomeProduto = editTextProduto.getText().toString();
+                String codigoProduto = editTextCodigoProduto.getText().toString();
+                String preco = editTextPreco.getText().toString();
+                String quantidade = editTextQuantidade.getText().toString();
+                //Verifica se os valores estão sendo capturados
+                Log.d("MainActivity", "Nome do Produto: " + nomeProduto + ", Código do Produto: " + codigoProduto + ", Preço: " + preco + ", Quantidade: " + quantidade);
+            });
+
     }
 }
